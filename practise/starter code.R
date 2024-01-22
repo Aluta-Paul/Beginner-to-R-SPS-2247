2d7cbe287380fb3a84a154c32226a829de5988ba
Aluta Paul <pnjujuna@gmail.com>
2024-01-22 20:30:08 +0300

    version 1

---
+++ practise/starter code.R
@@ -0,0 +1,29 @@
+
+# my first program in R programming
+
+ x = 13
+ print(x)
+ print(class(x))
+
+ alphabet ="a"
+ print(alphabet)
+print(class(alphabet))
+
+message = "welcome to my code"
+print(message)
+print(class(message))
+
+#changing value of a variable
+message = "must be nice being me!"
+print(message)
+
+
+#CONSTANTS -- we can declare constants using the <- symbol.
+
+x <- "hellow world"
+print(x)
+
+
+
+
+
